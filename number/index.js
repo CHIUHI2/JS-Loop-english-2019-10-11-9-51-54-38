470c4df952739784bcefce6d80de8bbb61d69ec7
Hins Chiu <chiuhi2@oocl.com>
2020-11-12 11:15:58 +0800

    update

--- number/index.js
+++ number/index.js
@@ -1,4 +1,8 @@
-for(let i = 1 ; i < 21 ; i++) {
+for(let i = 0 ; i <= 20 ; i++) {
+	if(i == 0) {
+		continue;
+	}
+
 	let str = i + ' is ' + (i % 2 == 0 ? 'even' : 'odd') + ' number.';
 	console.log(str);
 }
